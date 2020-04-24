b8f4ad93fd4f42b3b2ca38b79949cd1715ae1989
Yennie Jun <yennie.jun@gmail.com>
2020-04-23 23:11:35 -0500

    Style the list cafes page

--- api/client/src/App/pages/CafeList.js
+++ api/client/src/App/pages/CafeList.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { FaBeer } from 'react-icons/fa';
+import CafeListTable from './cafeListTable'
+import './CafeList.css';
 
 
 class CafeList extends Component {
@@ -14,14 +16,10 @@ class CafeList extends Component {
   async getCafes() {
     const response = await fetch(`/api/cafe`);
     if (response.status !== 200) throw Error(response);
-
     const body = await response.json();
-    
     console.log("get cafes", body)
-
     return body;
   };
-
 
   componentDidMount() {
     this.getCafes()
@@ -29,7 +27,6 @@ class CafeList extends Component {
   }
 
   convertIsPrivateBoolean(isPrivate) {
-    console.log("convertIsPrivateBoolean")
     if (isPrivate === true) {
       return "private"
     } else {
@@ -37,65 +34,17 @@ class CafeList extends Component {
     }
   }
 
-  // https://stackoverflow.com/questions/50385680/react-html-table
-  getTableContent(arr) {
-      console.log("HELLO?")
-      console.log("get table content", arr)
-
-      const tableStyle = {
-        // backgroundColor: "#FFFFE0",
-        borderCollapse: "collapse",
-        marginLeft: "auto",
-        marginRight: "auto",
-        textAlign: "center"
-      };
-
-
-      const tablebody = arr.map(function (item, i) {
-          return (
-            <tr key={item.id}>
-               <td style={{padding:"0.3rem"}}>{item.id}</td>
-               <td>{item.cafename}</td>
-               <td>{item.location}</td>
-               <td>{item.capacity}</td>
-               {console.log(item.isprivate)}
-               <td>{item.isprivate ? "private" : "public"}</td>
-               <td>{item.created_timestamp}</td>
-            </tr>
-          )
-      });
-
-      return (
-        <table style={tableStyle}>
-          <thead></thead>
-            <tbody>
-               <tr style={{fontWeight:700, borderBottom: "1px solid black"}}>
-                 <td style={{padding: "0.5rem 3rem 0.5rem"}}>Id</td>
-                 <td style={{padding: "0.5rem 3rem 0.5rem"}}>Name</td>
-                 <td style={{padding: "0.5rem 3rem 0.5rem"}}>Location</td>
-                 <td style={{padding: "0.5rem 3rem 0.5rem"}}>Capacity</td>
-                 <td style={{padding: "0.5rem 3rem 0.5rem"}}>Private?</td>
-                 <td style={{padding: "0.5rem 3rem 0.5rem"}}>Time created</td>
-              </tr>
-              {tablebody}
-            </tbody>
-          </table>
-        );
-  };
-
-
   render() {
     return (
-    <div className="App">
-      <h1>Existing cafes</h1>
+    <div className="cafeList">
+      <h1 className="cafeListTitle">Existing cafes</h1>
         {
           !!this.state.cafes && this.state.cafes.length > 0 && 
-          //console.log("render", this.state.cafes) &&
-            this.getTableContent(this.state.cafes)
+            <CafeListTable cafes={this.state.cafes} />
         }
 
       <Link to={'/'}>
-        <h3>Return to login page <FaBeer/></h3>
+        <h3 className="backToLogin">Return to login page <FaBeer/></h3>
       </Link>
     </div>
     );
---
+++ api/client/src/App/pages/cafeListTable.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import './CafeList.css';
+import { Link } from 'react-router-dom';
+import classNames from 'classnames';
+
+function CafeListTable(props) {
+
+	const tablebody = props.cafes.map(function (item, i) {
+    	return (
+	        <tr key={item.id}>
+	           <td >{item.id}</td>
+	           <td>{item.cafename}</td>
+	           <td>{item.location}</td>
+	           <td>{item.capacity}</td>
+	           {console.log(item.isprivate)}
+	           <td>{item.isprivate ? "private" : "public"}</td>
+	           <td>{item.created_timestamp}</td>
+	        </tr>
+	      )
+	  });
+
+  return (
+    <table className="list-table">
+          <thead></thead>
+            <tbody>
+               <tr>
+                 <td style={{}}>Id</td>
+                 <td>Name</td>
+                 <td>Location</td>
+                 <td>Capacity</td>
+                 <td>Private?</td>
+                 <td>Time created</td>
+              </tr>
+              {tablebody}
+            </tbody>
+          </table>
+  );
+}
+
+export default CafeListTable;
